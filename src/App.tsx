73728399c08ff28767d6c9218c1758dae2555de1
Matt Fisher <matt.fisher@nsidc.org>
2021-06-10 14:03:33 -0600

    Basic loading state reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import {
+  combineReducers,
+  Reducer,
+} from '@reduxjs/toolkit';
+
 import './style/App.css';
+import loadingState from './state/loading';
+
+const rootReducer = combineReducers({
+  loading: loadingState.reducer,
+});
+type RootReducerType = ReturnType<typeof rootReducer>;
+
 
 interface IAppProps {
   domElement: Element;
@@ -7,17 +19,24 @@ interface IAppProps {
 
 const App: React.FC<IAppProps> = (props) => {
   const animation = props.domElement.getAttribute('data-animation')
+  const [state, dispatch] = React.useReducer<Reducer<RootReducerType>>(
+    rootReducer,
+    // TODO: dedup initial state -- it's defined in the "createSlice" call too...
+    {
+      loading: true,
+    },
+  );
+
+  React.useEffect(() => {
+    if ( state.loading ) {
+      setTimeout(() => dispatch(loadingState.actions.loaded()), 5000);
+    }
+  });
 
   return (
     <div className="ros-animator-widget-container">
-      <header className="ros-animator-widget-header">
-
-        <p>
-          This is a test widget!
-        </p>
-
-      </header>
       <span>Content: {animation}</span>
+      <div>{JSON.stringify(state)}</div>
     </div>
   );
 }
---
+++ src/state/loading.ts
@@ -0,0 +1,19 @@
+import {
+  createSlice,
+} from '@reduxjs/toolkit';
+
+// TODO: DRY initialState. Why do we have to define it in createSlice _and_
+// useReducer?
+const initialState = true;
+
+const stateSlice = createSlice({
+  name: "loading",
+  initialState: initialState,
+  reducers: {
+    loaded: (prevState) => {
+      return false
+    },
+  },
+});
+
+export default stateSlice;
